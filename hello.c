af89ab3557b833d8f6725c79a6eda73562303ebb
Trevor <tahlers@unomaha.edu>
2022-08-26 13:40:17 +0000

    deleted some whitespace

--- hello.c
+++ hello.c
@@ -1,10 +1,15 @@
+/*   Trevor Ahlers
+*    tahlers@unomaha.edu
+*    2022,08,24
+*
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
-
-int main(int argc, char **argv) {
-
+int main(int argc, char **argv){
 
 printf("Trevor\n");
 printf("Computer Engineering!\n")
 
+return 0;
 }
